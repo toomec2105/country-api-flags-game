a2187856427155a9991e6b0f8c18878e2e3889ff
toomec2105 <tomec2105@interia.pl>
2019-12-28 15:16:38 +0100

    get countries from API

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Hello");
+// set constants for number of options, country array
+// vars: options(array which keeps indexes), correctAnswer(index), userAnswer(index),   
+ // https://restcountries.eu/
+const NUMBER_OF_OPTIONS = 3;
+const API_URL = "https://restcountries.eu/rest/v2/all"
+let countryArray;
+let options;
+let correctAnswer;
+let userAnswer;
+
+console.log("starting the program");
+init();
+
+async function init(){
+    countryArray = await getArray();
+    console.log("Store the countries");
+}
+
+
+
+async function getArray() {
+    console.log("Get array of country data from API");
+    try {
+        let response = await fetch(API_URL);
+        let countryArray = await response.json();
+        console.log(countryArray);
+        return countryArray;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
