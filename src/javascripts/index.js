69195090df94e4089db87c704d78be637c375be3
toomec2105 <tomec2105@interia.pl>
2020-05-25 12:03:33 +0200

    move arrays to a new file

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -12,6 +12,7 @@ import { renderResult } from "../module-view/renderResult";
 import { shuffle } from "../module-universal/array-utilities/shuffle";
 import { getUserAnswer } from "../module-view/getUserAnswer";
 import { getLevelItemsArrMap } from "../module-country-api/extractCountryNames";
+import { easyFlagsImmutable, mediumFlagsImmutable, hardFlagsImmutable } from "./immutableArrays";
 
 /* ----------------------- HTML elements -------------------------- */
 const flagImg = document.getElementById("flag");
@@ -49,21 +50,6 @@ let correctAnswer;
 let nextFlagAllowed = false;
 let difficulty = "medium";
 let indexOfAnswer = 0;
-const easyFlagsImmutable = ["Korea (Republic of)", "Netherlands", "Indonesia",
-    "Mayotte", "Antarctica", "Israel", "Canada", "Switzerland", "Brazil", "Japan",
-    "United Kingdom of Great Britain and Northern Ireland", "Sweden", "Turkey", "Germany", "United States of America",
-    "Spain", "Cyprus", "Slovakia", "Greece", "Austria", "Croatia", "Italy", "Denmark", "Russian Federation",
-    "Poland", "France", "China", "Uruguay", "Belgium", "Czech Republic", "Ukraine", "Holy See", "Norway", "Portugal", "Sudan", "Finland", "Nepal", "New Zeland", "Iceland", "United States Minor Outlying Islands"];
-const mediumFlagsImmutable = ["Australia", "Puerto Rico", "Korea (Democratic People's Republic of)",
-    "Mexico", "Macedonia (the former Yugoslav Republic of)",
-    "Saint Martin (French part)", "Malta", "Luxembourg", "Ireland", "Bulgaria", "Republic of Kosovo", "Iraq", "India",
-    "Egypt", "Chile", "Mongolia", "Lithuania", "Montenegro", "Viet Nam", "Jamaica",
-    "Slovenia", "Albania", "Hungary", "Belarus",
-    "Estonia", "Romania", "Saudi Arabia", "Nicaragua", "Venezuela (Bolivarian Republic of)", "Syrian Arab Republic", "Serbia", "Hong Kong", "Argentina"];
-const hardFlagsImmutable = ["Tunisia", "Liechtenstein", "Bosnia and Herzegovina", "Greenland", "Kenya", "Georgia", "Thailand", "Panama", "Jersey", "Bhutan", "Cambodia", "Tobago",
-    "Kuwait", "Haiti", "Algieria", "Lebanon", "Sri Lanka", "Libya", "Colombia", "Ecuador", "Paraguay", "Afghanistan", "San Marino", "Sudan", "Andora", "Senegal", "Somalia",
-    "Turkmenistan", "Pakistan", "Iran", "Peru", "Cuba", "Honduras", "Jordan", "Uzbekistan", "South Georgia and the South Sandwich Islands", "Papua New Guinea", "Cook Islands",
-    "Virgin Islands (British)", "Heard Island and McDonald Islands", "Western Sahara", "Åland Islands", "French Southern Territories", "Nigeria"];
 const masterFlagsImmutable = [];
 
 hasDuplicates(easyFlagsImmutable);
