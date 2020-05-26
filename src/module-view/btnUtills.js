a10385abfcf6069bb28d4989b195cb7427b8124d
toomec2105 <tomec2105@interia.pl>
2020-05-26 20:08:19 +0200

    make sure that argument in btnUtills is boolean

--- src/module-view/btnUtills.js
+++ src/module-view/btnUtills.js
@@ -1,6 +1,8 @@
 // maby maby sth like disableButtons(btn1, btn2);
 export function toggleRadioButtons(buttonsArray, isDisabled) {
-
+    if (typeof isDisabled != "boolean"){
+        throw Error("Second argument must be a boolean.");
+      }
     if (Array.isArray(buttonsArray)) {
         let i;
         for (i = 0; i < buttonsArray.length; i++) {
